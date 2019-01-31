ae61cee32eb9ea87f89eaf50e8b11f92b200f977
Tyler Vander Maas <tvmaasjazz@gmail.com>
2019-01-31 12:41:34 -0500

    refactor(filter): Add scheme to redirect

--- src/modules/filters/redirect-http-exception.filter.ts
+++ src/modules/filters/redirect-http-exception.filter.ts
@@ -1,5 +1,5 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-import { Response } from 'express';
+import { Response, Request } from 'express';
 import { BaseHttpExceptionFilter } from './base-http-exception.filter';
 import { RedirectException } from '../exceptions';
 
@@ -7,10 +7,17 @@ import { RedirectException } from '../exceptions';
 export class RedirectHttpExceptionFilter extends BaseHttpExceptionFilter implements ExceptionFilter {
     catch(exception: RedirectException, host: ArgumentsHost) {
         const res: Response = host.switchToHttp().getResponse();
+        const req: Request = host.switchToHttp().getRequest();
 
         // get the original exception if it was caught more than once
         exception = this.getInitialException(exception);
 
-        res.redirect(exception.redirectPath);
+        // prepend scheme to redirect if exists on request
+        let scheme = '';
+        if (req['customUrlScheme']) {
+            scheme = req['customUrlScheme'];
+        }
+
+        res.redirect(`${scheme}${exception.redirectPath}`);
     }
 }
