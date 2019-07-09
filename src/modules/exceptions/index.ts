d40dfb0832100d2091a7eea905183eb8d1f8d6aa
Josh Gibson <gibsonjoshua55@gmail.com>
2019-07-09 11:21:06 -0400

    refactor: removes NodeEventHandler

--- src/modules/exceptions/index.ts
+++ src/modules/exceptions/index.ts
@@ -10,5 +10,3 @@ export * from './redis.exception';
 export * from './sql.exception';
 export * from './unauthorized.exception';
 export * from './validation.exception';
-export * from './uncaught.exception';
-export * from './unhandled-promise.exception';
