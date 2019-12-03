ba5e960a6df3e3490f6665de18692b92f25813b1
Josh Gibson <gibsonjoshua55@gmail.com>
2019-12-03 16:25:33 -0500

    fix(validation): check array validity in identity array validation

--- src/modules/pipes/identity-array-validation.pipe.ts
+++ src/modules/pipes/identity-array-validation.pipe.ts
@@ -5,7 +5,13 @@ import { ValidationException } from '../exceptions';
 @Injectable()
 export class IdentityArrayValidationPipe implements PipeTransform<string[]> {
     async transform(values: string[] = []) {
+        if (!values) {
+            throw new ValidationException('array of identities is not defined');
+        }
         const validator = new Validator();
+        if (!validator.isArray(values)) {
+            throw new ValidationException('invalid array');
+        }
         values.forEach((value) => {
             if (!validator.isUUID(value)) {
                 throw new ValidationException('invalid uuid list of identities');
