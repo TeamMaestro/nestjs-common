fe682fec42e2ca244f5cb69b4141a7694c5124b6
John Pinkster <john.c.pinkster@gmail.com>
2022-06-14 11:50:20 -0400

    feat: mark all hive messages as critical

--- src/modules/decorators/rpc/hive-message.decorator.ts
+++ src/modules/decorators/rpc/hive-message.decorator.ts
@@ -1,12 +1,15 @@
+import { Critical } from '../critical.decorator';
+
 export const RPC_PATTERN_KEY = 'loop:rpc-pattern-key';
 export const RPC_OPTIONS_KEY = 'teamhive:rpc-message-options';
 
 export interface HiveMessageOptions {
-    isRpc?: boolean
+    isRpc?: boolean;
 }
 
 export function HiveMessage(messagePattern: string, options: HiveMessageOptions = {}): ClassDecorator {
     return (target) => {
+        Critical()(target);
         Reflect.defineMetadata(RPC_PATTERN_KEY, messagePattern, target);
         Reflect.defineMetadata(RPC_OPTIONS_KEY, options, target);
     };
